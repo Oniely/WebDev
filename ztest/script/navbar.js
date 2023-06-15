4d9eb5714337799c97ab75e066671e485168d273
Oniely <jabagatniel09@gmail.com>
2023-06-15 10:51:00 +0800

    update navbar

--- ztest/script/navbar.js
+++ ztest/script/navbar.js
@@ -3,19 +3,19 @@ const navToggle = document.querySelector('.nav-toggle');
 
 navToggle.addEventListener('click', function() {
 
-	if (!nav.classList.contains('show-link')) {
-		nav.classList.add('show-link');
-	} else {
-		nav.classList.remove('show-link')
-	}
+	
+	// toggle only works if your css has a visibility property
+	// for example if you click the burger (navToggle) it will
+	// be visibility: visible in the css by the show-link
+	nav.classList.toggle('show-link')
 
 })
 
-window.addEventListener('resize', function(x) {
+window.addEventListener('resize', function() {
 
-	var x = window.matchMedia('("max-width: 800px")')
-
-	if (x) {
+	var x = window.matchMedia('(min-width: 800px)')
+	// if window width is 800px above (min-width) then it will remove show-link
+	if (x.matches) {
 		nav.classList.remove('show-link')
 	}
 })
