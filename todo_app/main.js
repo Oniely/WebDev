b7833ad93bd88de362d9f8c7fe9b8fc2c2e5abbb
Oniely <jabagatniel09@gmail.com>
2023-06-24 22:31:41 +0800

    add localstorage for todolistapp

--- todo_app/main.js
+++ todo_app/main.js
@@ -9,39 +9,57 @@ document.addEventListener("DOMContentLoaded", () => {
 		<li>
 		`
 	}
+	
+	let value = [];
+	let items = localStorage.getItem('todo');
+
+	if (items !== "" && items !== null) {
+		let arrayItems = JSON.parse(items)
+
+		for (i in arrayItems) {
+			value.push(arrayItems[i])
+		}
+	}
+
+	for (i in value) {
+		document.querySelector('.todo').innerHTML += addList(value[i]);
+	}
 
 	form.onsubmit = () => {
 		const input = document.querySelector('#input').value;
 		const todoList = document.querySelector('.todo');
-
+		
 		todoList.innerHTML += addList(input);
-
+		
 		document.querySelector("#input").value = "";
 
 		const closeBtn = document.querySelectorAll('button');
 
-	closeBtn.forEach(btn => {
-		btn.addEventListener('click', () => {
-			const currentBtn = btn.parentElement;
+		closeBtn.forEach(btn => {
+			btn.addEventListener('click', () => {
+				const currentBtn = btn.parentElement;
 
-			currentBtn.style.display = "none";
+				currentBtn.style.display = "none";
+			})
 		})
-	})
 
-	const listItem = document.querySelectorAll('p');
+		const listItem = document.querySelectorAll('p');
 
-	listItem.forEach(item => {
-		item.addEventListener('click', () => {
-			if (!item.classList.contains('done')) {
-				item.classList.add('done')
-			} else {
-				item.classList.remove('done')
-			}
+		listItem.forEach(item => {
+			item.addEventListener('click', () => {
+				if (!item.classList.contains('done')) {
+					item.classList.add('done')
+				} else {
+					item.classList.remove('done')
+				}
+			})
 		})
-	})
 
+		value.push(input)
+		localStorage.setItem('todo', JSON.stringify(value))
 		return false;
 	}
+
 
 	const closeBtn = document.querySelectorAll('button');
 
